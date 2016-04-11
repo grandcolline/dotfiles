a0bb37d73d790c3ffd84820b535f7344d7899430
grandcolline <grandcolline@gmail.com>
2016-04-12 00:01:39 +0900

    committest

--- zprezto/modules/git-custom/alias.zsh
+++ zprezto/modules/git-custom/alias.zsh
@@ -68,7 +68,7 @@ alias gad='git add'
 alias gadp='git add -p' #hunkごと
 alias gcm='git commit -v'
 alias gcmm='git commit -m'
-alias gcma='git commit -a'
+alias gcma='git commit --amend --date="`date`"'
 
 # reset (rs)
 alias grs='git reset'
