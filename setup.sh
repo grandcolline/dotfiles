c7605daa763eba8634b075b0f7c1cf16a6f7236c
Taiki Hayashi <grandcolline@gmail.com>
2019-06-29 17:54:21 +0900

    みにまる！みにまる！

--- setup.sh
+++ setup.sh
@@ -2,32 +2,26 @@
 
 dir_path=`pwd`
 
-# git
 echo "===== Git setting ====="
 ln -vsfn $dir_path/config/git $XDG_CONFIG_HOME/git
 echo ""
 
-# fish
 echo "===== FishShell setting ====="
 ln -vsfn $dir_path/config/fish $XDG_CONFIG_HOME/fish
 echo ""
 
-# tmux
 echo "===== tmux setting ====="
 ln -vsf $dir_path/tmux.conf $HOME/.tmux.conf
 echo ""
 
-# nvim
 echo "===== NeoVim setting ====="
 ln -vsfn $dir_path/config/nvim $XDG_CONFIG_HOME/nvim
 echo ""
 
-# vim
 echo "===== Vim setting ====="
 ln -vsfn $dir_path/vimrc $HOME/.vimrc
 echo ""
 
-# vim
 echo "===== mycli setting ====="
 ln -vsfn $dir_path/myclirc $HOME/.myclirc
 echo ""
