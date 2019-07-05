4aee6169dbe1ecd1e8232993f69912eb6e5e1178
Taiki Hayashi <grandcolline@gmail.com>
2019-07-05 17:58:00 +0900

    なにぬねのーべんばー

--- setup.sh
+++ setup.sh
@@ -8,7 +8,13 @@ echo ""
 
 echo "===== FishShell setting ====="
 ln -vsfn $dir_path/config/fish $XDG_CONFIG_HOME/fish
-echo ""
+echo "install completion"
+curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/fish/docker.fish \
+	-o $dir_path/config/fish/completions/docker.fish
+curl https://raw.githubusercontent.com/docker/compose/master/contrib/completion/fish/docker-compose.fish \
+	-o $dir_path/config/fish/completions/docker-compose.fish
+curl https://raw.githubusercontent.com/evanlucas/fish-kubectl-completions/master/completions/kubectl.fish \
+	-o $dir_path/config/fish/completions/kubectl.fish
 
 echo "===== tmux setting ====="
 ln -vsf $dir_path/tmux.conf $HOME/.tmux.conf
