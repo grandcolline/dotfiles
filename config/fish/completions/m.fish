45faf774e295a5aacd2edc2aef0c252b40bdf1da
Taiki Hayashi <grandcolline@gmail.com>
2022-02-08 22:07:02 +0900

    refactor m function

--- config/fish/completions/m.fish
+++ config/fish/completions/m.fish
@@ -1,8 +1,14 @@
 function __complete_m
-  #    set -l words (commandline -pco)
-  #    set -l cword (count $words)
-  if test -f ".my/script"
-    .my/script "comp"
+  set -l words (commandline -pco)
+  set -l cword (count $words)
+
+  if test -f ".my/script.mjs"
+    switch $cword
+      case 1
+        .my/script.mjs "comp"
+      case '*'
+        __fish_complete_path
+    end
   end
 end
 
--- config/fish/functions/m.fish
+++ config/fish/functions/m.fish
@@ -1,6 +1,6 @@
 function m -d "Excute My Script!!"
-  if test -f ".my/script"
-    .my/script $argv
+  if test -f ".my/script.mjs"
+    .my/script.mjs $argv
   else
     echo "no script file!!"
   end
