d1fe3aa66401eb6b8703bacb9dfa0330ecd67aaa
Taiki Hayashi <grandcolline@gmail.com>
2022-04-27 08:11:04 +0900

    add gitlab setting

--- config/fish/functions/clone.fish
+++ config/fish/functions/clone.fish
@@ -2,6 +2,7 @@ function clone -d "git clone in GOPATH and move"
   set -l path ( echo "$argv" \
     | sed -e "s;https://;;g" \
     | sed -e "s;git@github.com:;github.com/;g" \
+    | sed -e "s;git@gitlab.com:;gitlab.com/;g" \
     | sed -e 's/.git$//g' \
   )
   git clone "$argv" $GOPATH/src/$path
