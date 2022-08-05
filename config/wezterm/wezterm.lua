6c6351186d364bf6adddfd38f8b098bea27e4c6f
Taiki Hayashi <grandcolline@gmail.com>
2022-08-05 21:31:21 +0900

    update wezterm config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,6 +1,12 @@
 local wezterm = require 'wezterm';
 return {
-  font = wezterm.font("JetBrainsMonoNL Nerd Font Mono"),
+  font = wezterm.font(
+    'JetBrainsMonoNL Nerd Font Mono',
+    {
+      weight = 'Regular',
+      -- weight = 'Thin',
+    }
+  ),
   font_size = 15.0,
   -- color_scheme = "Ayu Mirage",
   colors = {
@@ -26,8 +32,12 @@ return {
       "#4E7D78", -- cyan
       "#B7BBB7", -- white
     },
+    cursor_bg     = '#B7BBB7',
+    cursor_border = '#B7BBB7'
   },
-  window_background_opacity = 0.95,
+  -- window_background_opacity = 0.95,
+  hide_tab_bar_if_only_one_tab = true,
+
   -- フォントサイズ変更時にウィンドウサイズも変更されないようにする
   adjust_window_size_when_changing_font_size = false,
 }
