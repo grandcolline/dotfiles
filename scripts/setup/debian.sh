c521b6f6a0938d273d44af5e89d6128e11a8d999
Taiki Hayashi <grandcolline@gmail.com>
2023-01-28 21:20:21 +0900

    update comments

--- scripts/setup/debian.sh
+++ scripts/setup/debian.sh
@@ -28,8 +28,8 @@ Install fd-find
 Install expect
 # Install jq
 
+# Get LATEST version fish. refs: https://software.opensuse.org/download.html?project=shells%3Afish%3Arelease%3A3&package=fish
 RecInfo "install fish"
-# 最新バーションを取得 refs: https://software.opensuse.org/download.html?project=shells%3Afish%3Arelease%3A3&package=fish
 RecExec echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
 RecExec curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
 RecExec sudo apt -y update
