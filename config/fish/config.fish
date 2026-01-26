27075964e04668b6b30489f35b57bf4bcf7a047b
Taiki Hayashi <grandcolline@gmail.com>
2026-01-26 22:14:52 +0900

    🎫

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -146,6 +146,38 @@ end
 
 
 # ----------------------------
+#  Theme (Syntax Highlighting)
+# ----------------------------
+set -g fish_color_autosuggestion 555 brblack
+set -g fish_color_cancel -r
+set -g fish_color_command 005fd7
+set -g fish_color_comment 990000
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_end 009900
+set -g fish_color_error ff0000
+set -g fish_color_escape 00a6b2
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_host_remote yellow
+set -g fish_color_normal normal
+set -g fish_color_operator 00a6b2
+set -g fish_color_param 00afff
+set -g fish_color_quote 999900
+set -g fish_color_redirection 00afff
+set -g fish_color_search_match white --background=brblack
+set -g fish_color_selection white --bold --background=brblack
+set -g fish_color_status red
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
+set -g fish_pager_color_completion
+set -g fish_pager_color_description B3A06D yellow
+set -g fish_pager_color_prefix normal --bold --underline
+set -g fish_pager_color_progress brwhite --background=cyan
+set -g fish_pager_color_selected_background -r
+
+
+# ----------------------------
 #  Key Binds
 # ----------------------------
 bind \cr '__fzf_history'
